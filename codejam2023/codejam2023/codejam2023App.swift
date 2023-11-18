f700ea66a68707cca7852fdb91937021c130e953
DashiellMR <dashiellrich@gmail.com>
2023-11-18 13:32:50 -0500

    added swift

---
+++ codejam2023/codejam2023/codejam2023App.swift
@@ -0,0 +1,17 @@
+//
+//  codejam2023App.swift
+//  codejam2023
+//
+//  Created by Dashiell Rich on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct codejam2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
